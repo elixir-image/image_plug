4e08829e97fdceef99a5d020bb79c347eb7ba1da
Kip Cole <kipcole9@gmail.com>
2023-01-15 09:30:20 +0000

    Initial commit

---
+++ lib/plug_image.ex
@@ -0,0 +1,18 @@
+defmodule PlugImage do
+  @moduledoc """
+  Documentation for `PlugImage`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugImage.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_image_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugImageTest do
+  use ExUnit.Case
+  doctest PlugImage
+
+  test "greets the world" do
+    assert PlugImage.hello() == :world
+  end
+end
