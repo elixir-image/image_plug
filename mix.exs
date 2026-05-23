a5d9e3849c25e3cd320109672987bff266158fc0
Kip Cole <kipcole9@gmail.com>
2026-05-23 11:50:35 +1000

    Add json_polyfill when required

--- mix.exs
+++ mix.exs
@@ -47,7 +47,15 @@ defmodule Image.Plug.MixProject do
       {:ex_doc, "~> 0.34", only: [:dev, :release], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
-    ]
+    ] ++ maybe_json_polyfill()
+  end
+
+  defp maybe_json_polyfill do
+    if Code.ensure_loaded?(:json) do
+      []
+    else
+      [{:json_polyfill, "~> 0.2 or ~> 1.0"}]
+    end
   end
 
   defp description do
